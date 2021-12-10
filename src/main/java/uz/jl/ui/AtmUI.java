e61b4c77891171c3ecca9ba1829408ecb9cb2180
saidjalol123 <saidjalolqodirov1@gail.com>
2021-12-10 11:30:12 +0300

    hr crud added

--- src/main/java/uz/jl/ui/AtmUI.java
+++ src/main/java/uz/jl/ui/AtmUI.java
@@ -1,5 +1,16 @@
 package uz.jl.ui;
 
+import uz.jl.dao.db.FRWAuthUser;
+import uz.jl.enums.auth.Role;
+import uz.jl.enums.auth.UserStatus;
+import uz.jl.models.auth.AuthUser;
+import uz.jl.utils.Color;
+import uz.jl.utils.Input;
+import uz.jl.utils.Print;
+
+import java.util.List;
+import java.util.Objects;
+
 /**
  * @author Elmurodov Javohir, Wed 12:11 PM. 12/8/2021
  */
