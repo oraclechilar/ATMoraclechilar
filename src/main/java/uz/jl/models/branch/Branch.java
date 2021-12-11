b662de24c06e2050a3005bf5558b4ddb8a1609f4
saidjalol123 <saidjalolqodirov1@gail.com>
2021-12-11 08:48:49 +0300

    Branch service CRUD

--- src/main/java/uz/jl/models/branch/Branch.java
+++ src/main/java/uz/jl/models/branch/Branch.java
@@ -1,9 +1,12 @@
 package uz.jl.models.branch;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import uz.jl.enums.atm.Status;
 import uz.jl.models.Auditable;
+
+import java.util.Date;
 
 @Getter
 @Setter
@@ -11,4 +14,13 @@ public class Branch extends Auditable {
     private String name;
     private Status status;
     private String bankId;
+
+    @Builder(builderMethodName = "childBuilder", buildMethodName = "childBuild")
+
+    public Branch(Date createdAt, String createdBy, Date updatedAt, String updatedBy, int deleted, String name, Status status, String bankId) {
+        super(createdAt, createdBy, updatedAt, updatedBy, deleted);
+        this.name = name;
+        this.status = status;
+        this.bankId = bankId;
+    }
 }
--- src/main/java/uz/jl/ui/BranchUI.java
+++ src/main/java/uz/jl/ui/BranchUI.java
@@ -1,6 +1,19 @@
 package uz.jl.ui;
 
+import uz.jl.configs.Session;
+import uz.jl.dao.db.FRWBranch;
+import uz.jl.enums.atm.Status;
+import uz.jl.enums.http.HttpStatus;
+import uz.jl.models.branch.Branch;
+import uz.jl.response.ResponseEntity;
+import uz.jl.services.branchService.BranchService;
+import uz.jl.utils.Color;
 import uz.jl.utils.Input;
+import uz.jl.utils.Print;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Elmurodov Javohir, Wed 12:11 PM. 12/8/2021
@@ -8,6 +21,16 @@ import uz.jl.utils.Input;
 public class BranchUI {
     public static void create() {
         String name = Input.getStr("Branch name :");
+        Branch branch = getBranch(name);
+        if (Objects.nonNull(branch)) {
+            Print.println("This branch is available");
+            return;
+        }
+        branch = Branch.childBuilder().name(name).status(Status.ACTIVE).
+                bankId(Session.getInstance().getUser().getBankId()).childBuild();
+        ResponseEntity<String> response = BranchService.create(branch);
+        if (response.getStatus().equals(HttpStatus.HTTP_201.getCode()))
+            Print.println(response.getData());
     }
 
     public static void update() {
@@ -15,22 +38,64 @@ public class BranchUI {
     }
 
     public static void delete() {
-
+        list();
+        String name = Input.getStr("name -> ");
+        ResponseEntity<String> response = BranchService.delete(name);
+        if (response.getStatus().equals(HttpStatus.HTTP_400.getCode()))
+            Print.println(Color.RED, response.getData());
+        else Print.println(Color.PURPLE, response.getData());
     }
 
     public static void list() {
-
+        ResponseEntity<ArrayList<Branch>> response = BranchService.list();
+        if (response.getStatus().equals(HttpStatus.HTTP_204.getCode())) {
+            Print.println(Color.RED, "There are no branch");
+        }
+        int i = 1;
+        for (Branch branch : response.getData()) {
+            Print.println(String.format("""
+                            %s -> name: %s""",
+                    i++, branch.getName()));
+        }
     }
 
     public static void block() {
-
+        list();
+        String str = Input.getStr("branch name : ");
+        ResponseEntity<String> response = BranchService.block(str);
+        if (response.getStatus().equals(HttpStatus.HTTP_400.getCode()))
+            Print.println(Color.RED, response.getData());
+        else Print.println(Color.PURPLE, response.getData());
     }
 
     public static void unblock() {
-
+        blockList();
+        String str = Input.getStr("branch name : ");
+        ResponseEntity<String> response = BranchService.unblock(str);
+        if (response.getStatus().equals(HttpStatus.HTTP_400.getCode()))
+            Print.println(Color.RED, response.getData());
+        else Print.println(Color.PURPLE, response.getData());
     }
 
     public static void blockList() {
+        ResponseEntity<ArrayList<Branch>> response = BranchService.list();
+        if (response.getStatus().equals(HttpStatus.HTTP_204.getCode())) {
+            Print.println(Color.RED, "There are no HR");
+        }
+        int i = 1;
+        for (Branch branch : response.getData()) {
+            if (branch.getStatus().equals(Status.BLOCKED))
+                Print.println(String.format("""
+                                %s -> Username: %s""",
+                        i++, branch.getName()));
+        }
+    }
 
+    private static Branch getBranch(String name) {
+        for (Branch branch : FRWBranch.getInstance().getAll()) {
+            if (branch.getName().equalsIgnoreCase(name))
+                return branch;
+        }
+        return null;
     }
 }
