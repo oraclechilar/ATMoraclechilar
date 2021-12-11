0071b0a7e52136ceeac2a0b3b18a6a7c6983bf2d
saidjalol123 <saidjalolqodirov1@gail.com>
2021-12-11 21:43:57 +0300

    Branch service and admin service conets💪

--- src/main/java/uz/jl/ui/AuthUI.java
+++ src/main/java/uz/jl/ui/AuthUI.java
@@ -24,24 +24,25 @@ public class AuthUI extends BaseUI {
     }
 
     public static void logout() {
-       service.logout();
+        service.logout();
         AuthUserDao.getInstance().writeAll();
     }
+
     public static void register() {
         String username = Input.getStr("username : ");
         String password = Input.getStr("password : ");
-        String serial=Input.getStr("password serial :");
-        String number=Input.getStr("password number : ");
-        String gender=Input.getStr("gender :");
-        String firstName=Input.getStr("firstname : ");
-        String lastName=Input.getStr("lastname : ");
-        String fatherName=Input.getStr("father name : ");
-        ResponseEntity<String> response=service.register(username,password,serial,
-                number,gender,firstName,lastName,fatherName);
+        String serial = Input.getStr("password serial :");
+        String number = Input.getStr("password number : ");
+        String gender = Input.getStr("gender :");
+        String firstName = Input.getStr("firstname : ");
+        String lastName = Input.getStr("lastname : ");
+        String fatherName = Input.getStr("father name : ");
+        ResponseEntity<String> response = service.register(username, password, serial,
+                number, gender, firstName, lastName, fatherName);
     }
 
     public static void profile() {
-        ResponseEntity<String> response=service.profile();
+        ResponseEntity<String> response = service.profile();
         showResponse(response);
     }
 }
--- src/main/java/uz/jl/ui/menus/Menu.java
+++ src/main/java/uz/jl/ui/menus/Menu.java
@@ -16,27 +16,33 @@ import java.util.Map;
 public class Menu {
     public static Map<String, MenuKey> menus() {
         Role role = Session.getInstance().getUser().getRole();
-        UserStatus userStatus =Session.getInstance().getUser().getStatus();
+        UserStatus userStatus = Session.getInstance().getUser().getStatus();
         Map<String, MenuKey> menus = new LinkedHashMap<>();
         // TODO: 12/8/2021 do translations here
-        if (Role.SUPER_ADMIN.equals(role)&&userStatus.equals(UserStatus.ACTIVE)) {
+        if (Role.SUPER_ADMIN.equals(role) && userStatus.equals(UserStatus.ACTIVE)) {
             menus.put("Create Branch", MenuKey.CREATE_BRANCH);
             menus.put("Create Admin", MenuKey.CREATE_ADMIN);
 
             menus.put("Block Admin", MenuKey.BLOCK_ADMIN);
             menus.put("Block Branch", MenuKey.BLOCK_BRANCH);
 
+            menus.put("List admin", MenuKey.LIST_ADMIN);
+            menus.put("Block list admin", MenuKey.BLOCK_LIST_ADMIN);
+
             menus.put("Delete Admin", MenuKey.DELETE_ADMIN);
             menus.put("Delete Branch", MenuKey.DELETE_BRANCH);
 
             menus.put("Update Branch", MenuKey.UPDATE_BRANCH);
             menus.put("Profile", MenuKey.PROFILE);
-        } else if (Role.ADMIN.equals(role)&&userStatus.equals(UserStatus.ACTIVE)) {
+        } else if (Role.ADMIN.equals(role) && userStatus.equals(UserStatus.ACTIVE)) {
             menus.put("Create BRANCH", MenuKey.CREATE_BRANCH);
             menus.put("Delete BRANCH", MenuKey.DELETE_BRANCH);
 
             menus.put("Update BRANCH", MenuKey.UPDATE_BRANCH);
             menus.put("Create HR", MenuKey.CREATE_HR);
+
+            menus.put("List HR", MenuKey.LIST_HR);
+            menus.put("Block list HR", MenuKey.BLOCK_LIST_HR);
 
             menus.put("Delete HR", MenuKey.DELETE_HR);
             menus.put("Create ATM", MenuKey.CREATE_ATM);
@@ -45,8 +51,10 @@ public class Menu {
             menus.put("Block ATM", MenuKey.BLOCK_ATM);
 
             menus.put("Un Block ATM", MenuKey.UN_BLOCK_ATM);
-        } else if (role.in(Role.ADMIN, Role.HR)&&userStatus.equals(UserStatus.ACTIVE)) {
+        } else if (role.in(Role.ADMIN, Role.HR) && userStatus.equals(UserStatus.ACTIVE)) {
             menus.put("Employee List", MenuKey.LIST_EMPLOYEE);
+            menus.put("Block Employee List", MenuKey.BLOCK_LIST_EMPLOYEE);
+
             menus.put("Create Employee", MenuKey.CREATE_EMPLOYEE);
 
             menus.put("Delete Employee", MenuKey.DELETE_EMPLOYEE);
@@ -54,15 +62,15 @@ public class Menu {
 
             menus.put("Un block Employee", MenuKey.UN_BLOCK_EMPLOYEE);
             menus.put("Blocked Employee List", MenuKey.BLOCK_LIST_EMPLOYEE);
-        } else if (Role.EMPLOYEE.equals(role)&&userStatus.equals(UserStatus.ACTIVE)) {
+        } else if (Role.EMPLOYEE.equals(role) && userStatus.equals(UserStatus.ACTIVE)) {
             menus.put("Blocked Employee List", MenuKey.BLOCK_LIST_ATM);
             menus.put("Update Atm", MenuKey.UPDATE_ATM);
 
             menus.put("Atm List", MenuKey.LIST_ATM);
-        } else if (Role.ANONYMOUS.equals(role)||userStatus.equals(UserStatus.NON_ACTIVE)) {
+        } else if (Role.ANONYMOUS.equals(role) || userStatus.equals(UserStatus.NON_ACTIVE)) {
             menus.put("Login", MenuKey.LOGIN);
         }
-        if (!Role.ANONYMOUS.equals(role)&&userStatus.equals(UserStatus.ACTIVE)) {
+        if (!Role.ANONYMOUS.equals(role) && userStatus.equals(UserStatus.ACTIVE)) {
             menus.put("Logout", MenuKey.LOGOUT);
         }
         menus.put("Quit", MenuKey.EXIT);
